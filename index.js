dc5e9f9e5267136756a2317ea2bac19c4b9403f3
karyntok <karyntok@hotmail.com>
2025-12-29 16:08:23 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function callUs(){
+    const p = document.getElementById("callUs");
+    p.innerHTML = "Give Us A Call : 09-3020889";
+}
+
+function messageUs(){
+    const p = document.getElementById('messageUs');
+    p.innerHTML="Email us : Stephenphuan@gmail.com , We will get back to you as soon as possible.";
+}
